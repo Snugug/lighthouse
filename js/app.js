db35feabae384a663aa669af07c7d5c124928045
Eric J. Duran <eric.duran7@gmail.com>
2013-05-13 11:17:46 -0400

    Adding js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 
 // Our LH app.
 var app = angular.module('lighthouse', []);
-app.controller('WorkOrderCtrl', function ($scope) {
+
+
+// Our hack up paths because of the generator issue to work around.
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/work', {
+      templateUrl: 'partials/workorder.html',
+      controller: WorkOrderCtrl
+    }).
+    when('/request/new', {
+      templateUrl: 'partials/request.html',
+      controller: WorkOrderCtrl
+    }).
+    when('/outage/new', {
+      templateUrl: 'partials/outage.html',
+      controller: WorkOrderCtrl
+    }).
+    when('/problem/new', {
+      templateUrl: 'partials/problem.html',
+      controller: WorkOrderCtrl
+    }).
+    otherwise({redirectTo: '/work'});
+}]);
+
+function WorkOrderCtrl ($scope, $routeParams) {
   console.log($scope);
-});
+};
+
+function WorkOrderDetailCtrl($scope, $routeParams) {
+
+}
